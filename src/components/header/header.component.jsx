3d4693bbf5256602ee39ccc496e1db7ff79234d7
Adi Matei <ady_matey@yahoo.com>
2020-10-31 23:27:14 +0200

    added sign-up

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,7 +8,7 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.style.scss';
 
 const Header = ({ currentUser }) => {
-  console.log(currentUser);
+  // console.log(currentUser);
   return (
     <div className='header'>
       <Link className='logo-container' to='/'>
